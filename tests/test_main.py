331cb956bc51ddb11358760b2178472a1add179a
Aleksei Anikanov <python_work@bk.ru>
2024-08-29 21:19:14 +0300

    15_1 done

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 import pytest
 
-from src.main import Category, Product  # Предполагается, что ваш код находится в файле product.py
+from src.main import Category, Product
 
 
-def test_product_creation():
+def test_product_initialization():
     product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     assert product.name == "Samsung Galaxy S23 Ultra"
     assert product.description == "256GB, Серый цвет, 200MP камера"
@@ -13,81 +13,53 @@ def test_product_creation():
 
 def test_product_price_setter():
     product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product.price = 800
-    assert product.price == 800
-
-    product.price = -100
-    assert product.price == 800  # Цена не должна измениться
-
-    product.price = 0
-    assert product.price == 800  # Цена не должна измениться
+    product.price = 200000.0
+    assert product.price == 200000.0
+    product.price = -100.0
+    assert product.price == 200000.0  # Цена не должна измениться на отрицательную
 
 
-def test_new_product_classmethod():
-    product_data = {
-        "name": "Samsung Galaxy S23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5,
-    }
-    new_product = Product.new_product(product_data)
-    assert new_product.name == "Samsung Galaxy S23 Ultra"
-    assert new_product.description == "256GB, Серый цвет, 200MP камера"
-    assert new_product.price == 180000.0
-    assert new_product.quantity == 5
+def test_product_str():
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    assert str(product) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
 
 
-def test_category_creation():
+def test_product_addition():
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    assert product1 + product2 == 180000.0 * 5 + 210000.0 * 8
 
-    category = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
-    )
+
+def test_category_initialization():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    category = Category("Смартфоны",
+                        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                        [product1, product2])
     assert category.name == "Смартфоны"
-    assert (
-        category.description
-        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    )
+    assert category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     assert Category.category_count == 1
-    assert Category.product_count == 3
+    assert Category.product_count == 2
 
 
-def test_add_product_to_category():
+def test_category_add_product():
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
-    )
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category.add_product(product4)
-    assert Category.product_count == 7
+    category = Category("Смартфоны",
+                        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                        [product1])
+    category.add_product(product2)
+    assert Category.product_count == 4
 
 
-def test_category_products_property():
+def test_category_str():
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
-    )
-    expected_output = (
-        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
-        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
-        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
-    )
-    assert category.products == expected_output
+    category = Category("Смартфоны",
+                        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                        [product1, product2])
+    assert str(category) == "Смартфоны, количество продуктов: 13 шт."
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     pytest.main()
